3494d880d9548a57a0254332fe23fa48f1d04c03
Journey Cruz <journey.cruz@gmail.com>
2021-09-29 17:24:57 -0500

    init tailwindcss

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -2,14 +2,17 @@ import { Link } from "gatsby";
 import React from "react";
 import Icon from "./icon";
 import { cn } from "../lib/helpers";
+import { Disclosure } from "@headlessui/react";
 
 import * as styles from "./header.module.css";
 
-const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
-  <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <div className={styles.branding}>
-        <Link to="/">Home</Link>
+const Header = ({ onHideNav, onShowNav, showNav }) => (
+  <div className="relative z-100 bg-black text-white">
+    <div className="box-border my-0 mx-auto max-w-screen-lg p-4 flex sm:p-6">
+      <div className="font-semibold flex-1">
+        <Link className="inline-block p-2 text-gray-300 no-underline hover:text-white" to="/">
+          Home
+        </Link>
       </div>
 
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
