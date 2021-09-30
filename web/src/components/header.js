e52b7c4fac2b6417cb9258bb1e8b59433f4fd4f3
Journey Cruz <journey.cruz@gmail.com>
2021-09-29 22:06:06 -0500

    Update header.js

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -9,7 +9,6 @@ import { ChevronDownIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const navigation = [
   { name: "About", href: "/", current: false },
-  { name: "Projects", href: "/archive/", current: false },
   { name: "Services", href: "/archive/", current: false },
   { name: "Contact", href: "/archive/", current: false }
 ];
@@ -69,6 +68,74 @@ const Header = ({ pfp }) => (
                       {item.name}
                     </Link>
                   ))}
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="ml-3 relative z-50">
+                    <div>
+                      <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="https://live.staticflickr.com/65535/51534432635_edb2b3968f_k.jpg"
+                          alt=""
+                        />
+                        <ChevronDownIcon
+                          className="text-gray-400 my-auto mx-1 h-3 w-3 hover:text-white"
+                          aria-hidden="true"
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              LinkedIn
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              GitHub
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Dribbble
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
                 </div>
               </div>
             </div>
@@ -85,7 +152,11 @@ const Header = ({ pfp }) => (
                 <div>
                   <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                     <span className="sr-only">Open user menu</span>
-                    <img className="h-8 w-8 rounded-full" src='./static/JourneyPFP.JPEG' alt="" />
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src="https://live.staticflickr.com/65535/51534432635_edb2b3968f_k.jpg"
+                      alt=""
+                    />
                     <ChevronDownIcon
                       className="text-gray-400 my-auto mx-1 h-3 w-3 hover:text-white"
                       aria-hidden="true"
@@ -147,26 +218,101 @@ const Header = ({ pfp }) => (
             </div>
           </div>
         </div>
-
-        <Disclosure.Panel className="sm:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1">
-            {navigation.map(item => (
-              <Link
-                key={item.name}
-                to={item.href}
-                className={classNames(
-                  item.current
-                    ? "bg-gray-900 text-white"
-                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                  "block px-3 py-2 rounded-md text-base font-medium"
-                )}
-                aria-current={item.current ? "page" : undefined}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-        </Disclosure.Panel>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Disclosure.Panel className="static sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map(item => (
+                <Link
+                  key={item.name}
+                  to={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Link>
+              ))}
+              {/* Profile dropdown */}
+              <Menu as="div" className="ml-3 relative z-50">
+                <div>
+                  <Menu.Button className="bg-gray-800 flex w-full py-1 rounded-md hover:bg-gray-700">
+                    <span className="sr-only">Open user menu</span>
+                    <span className="text-gray-300 font-medium space-x-4 block text-base">
+                      Projects
+                    </span>
+                    <ChevronDownIcon
+                      className="text-gray-400 my-auto mx-1 h-3 w-3 hover:text-white"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="origin-top-left absolute left-0 mt-2 w-full rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          to="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          JavaScript
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          to="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          React
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          to="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Next.js
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
+          </Disclosure.Panel>
+        </Transition>
       </>
     )}
   </Disclosure>
